46490171c356c5ef4b5cbbee5570dd66d0ffd758
Halo333X <124174275+Halo333X@users.noreply.github.com>
2024-04-23 22:22:05 -0600

    Delete src/commands directory

--- src/commands/getDeletedMessage.ts
+++
@@ -1,9 +0,0 @@
-import { Message } from "whatsapp-web.js";
-
-let active = true;
-
-export function getDeletedMessage(after: Message, before: Message | undefined | null) {
-    if (before && active) {
-        after.reply('🔵 *Deleted Message*\n ✉️ *Content*: ' + `*${before.body}*`);
-    }
-}
